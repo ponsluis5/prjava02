cba2f2f3bfd217c97e9bc76240586ebada6c12b5
Sergi Pons <ponsluis5@gmail.com>
2014-04-23 13:41:38 +0200

    versió 3

--- src/prjava02/Prjava02.java
+++ src/prjava02/Prjava02.java
@@ -20,6 +20,7 @@ System.out.println("Nom de l'usuari: " + System.getProperty("user.name"));
 System.out.println("Carpeta Personal: " + System.getProperty("user.home")); 
 System.out.println("Sistema operatiu: " + System.getProperty("os.name"));
 System.out.println("Versió OS: " + System.getProperty("os.version"));
+System.out.println("Arquitecura OS: " + System.getProperty("os.arch"));
 }
 catch (IOException e) { e.printStackTrace();
 }}}
